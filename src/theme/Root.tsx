3e2aa3ad3c20d07386d0d66dc854c9c35614d43a
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-02-03 11:43:23 +0700

    fix: relative imports

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { QueryProvider } from '../context/QueryProvider';
+import { QueryProvider } from '@site/src/context/QueryProvider';
 // eslint-disable-next-line import/no-unresolved
 import 'windi-components.css';
 // eslint-disable-next-line import/no-unresolved
